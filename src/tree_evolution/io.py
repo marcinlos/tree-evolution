cfc5df633a75a79d954eddb81423f1cf7a5c9626
Marcin Łoś <marcin.los.91@gmail.com>
2026-03-03 03:30:15 +0100

    Ensure consistent device in activation plotting

--- src/tree_evolution/io.py
+++ src/tree_evolution/io.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import papermill as pm
 import torch
 
@@ -37,12 +38,22 @@ def run_notebook(notebook, output, params):
 
 def plot_activations(activations, bounds=(-1, 1), n=100):
     x0, x1 = bounds
-    x = torch.linspace(x0, x1, n, requires_grad=False)
+    device = _extract_device(activations[0])
+    x = np.linspace(x0, x1, n)
+    x_torch = torch.tensor(x, requires_grad=False).to(device)
 
     fig, axs = plt.subplots(1, len(activations), figsize=(12, 4))
     for mod, ax in zip(activations, axs, strict=True):
-        y = mod(x)
-        ax.plot(x.numpy(), y.detach().numpy())
+        y = mod(x_torch)
+        ax.plot(x, _numpify(y))
 
     plt.show()
     plt.close(fig)
+
+
+def _numpify(x):
+    return x.detach().cpu().numpy()
+
+
+def _extract_device(activation):
+    return next(activation.parameters()).device
